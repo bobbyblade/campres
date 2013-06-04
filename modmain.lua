2b0ed819786fe1ee01857acd08670c620dfca16a
Robert Sokoloski <bobbyblade@gmail.com>
2013-06-03 22:58:59 -0500

    Updated to find closest firepit

--- modmain.lua
+++ modmain.lua
@@ -7,12 +7,28 @@ function freerespostinit(inst)
 	print "campres freeres"
 	inst.oldresdo = inst.DoResurrect
 	inst.oldrescan = inst.CanResurrect
+
+	inst.FindClosestFreeResurrector = function(self)
+		local res = nil
+		local closest_dist = 0
+		for k,v in pairs(GLOBAL.Ents) do
+			if v.components.freeresurrector then
+				local dist = v:GetDistanceSqToInst(self.inst)
+				if not res or dist < closest_dist then
+					res = v
+					closest_dist = dist
+				end
+			end
+		end
+		return res
+	end
+	
 	inst.CanResurrect = function (self)
 		self:oldrescan()
 		
-		local res = TheSim:FindFirstEntityWithTag("freeresurrector")
-		if res and res.components.freeresurrector then
-			print "free camp res can"
+		local res = self:FindClosestFreeResurrector()
+	
+		if res then
 			return true
 		end
 		
@@ -20,7 +36,8 @@ function freerespostinit(inst)
 	
 	inst.DoResurrect = function(self)
 		self:oldresdo()
-		local res = TheSim:FindFirstEntityWithTag("freeresurrector")
+		local res = self:FindClosestFreeResurrector()
+	
 		if res and res.components.freeresurrector then
 			print "free camp res do"
 			res.components.freeresurrector:Resurrect(self.inst)
